22d8a90910f9a420bc1e7d6c70ed0b0974d5627c
Ghazanfar <ghazanfar.ali@packagex.app>
2019-06-10 22:15:12 +0500

    Added listing for users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UsersComponent } from "./users/users.component";
-import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserDetailComponent } from "./user-detail/user-detail.component";
+import { MaterialComponents } from "./MaterialComponents.module";
 
 @NgModule({
   declarations: [AppComponent, UsersComponent, UserDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialComponents
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
