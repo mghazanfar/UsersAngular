b52d8bdb0772c3f59d5ab8563208183ae4416b9f
Ghazanfar <ghazanfar.ali@packagex.app>
2019-06-11 04:14:11 +0500

    Added support for infinite scroll

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,13 +8,31 @@ import axios from "axios";
 })
 export class UsersComponent implements OnInit {
   public users = [];
+  public page = 1;
+  public noMore = false;
   constructor() {}
 
   ngOnInit() {
     axios
-      .get("https://api.github.com/users?since=135")
+      .get(`https://api.github.com/users?page=${this.page}`)
       .then(res => {
         this.users = res.data;
+        this.page = 2;
+        if (res.data.length < 30) {
+          this.noMore = true;
+        }
+      })
+      .catch(err => {});
+  }
+  fetchMore() {
+    axios
+      .get(`https://api.github.com/users?page=${this.page}`)
+      .then(res => {
+        this.users = this.users.concat(res.data);
+        this.page = this.page + 1;
+        if (res.data.length < 30) {
+          this.noMore = true;
+        }
       })
       .catch(err => {});
   }
