847a63203d5efb85e2237f38c4671bd743d3cf38
Ghazanfar <ghazanfar.ali@packagex.app>
2019-06-11 19:39:06 +0500

    Adding edit

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -36,6 +36,13 @@ export class UserDetailComponent implements OnInit {
   matcher = new MyErrorStateMatcher();
   public detail: { login: string; id: string }[] = [{ login: "", id: "" }];
   public edit: Boolean = false;
+  public editedDetail: {
+    email: string;
+    name: string;
+    location: string;
+    userType: string;
+  };
+  public emailError: Boolean = false;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -54,5 +61,40 @@ export class UserDetailComponent implements OnInit {
 
   onCancelEdit() {
     this.edit = false;
+  }
+
+  onSave() {
+    const id = this.route.snapshot.paramMap.get("id");
+    let name = `userDetail${id}`;
+    window.localStorage.setItem(
+      `userDetail${id}`,
+      JSON.stringify(this.editedDetail)
+    );
+    alert(
+      `User edited successfully with the following details: ${JSON.stringify(
+        this.editedDetail
+      )}`
+    );
+  }
+
+  handleChange(e) {
+    let regex = /^(?=.{8,32}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).*/;
+    let value: string = e.currentTarget.value;
+    let name: string = e.srcElement.name;
+    if (name === "name") {
+      this.editedDetail.name = value;
+    }
+    if (name === "location") {
+      this.editedDetail.location = value;
+    }
+    if (name === "userType") {
+      this.editedDetail.userType = value;
+    }
+    if (name === "email" && !regex.test(value)) {
+      this.emailError = true;
+    } else if (name === "email" && regex.test(value)) {
+      this.emailError = false;
+      this.editedDetail.email = value;
+    }
   }
 }
